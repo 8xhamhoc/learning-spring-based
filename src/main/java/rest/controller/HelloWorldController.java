153fdb970c45d1720dc47eca6e2d2046e8ed8481
8xhamhoc <tomatoquang@gmail.com>
2018-11-25 17:27:37 +0700

    spring annotation configuration

--- src/main/java/rest/controller/HelloWorldController.java
+++ src/main/java/rest/controller/HelloWorldController.java
@@ -1,24 +1,18 @@
 package rest.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping("/")
 public class HelloWorldController {
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String sayHello(ModelMap model) {
-        model.addAttribute("greeting", "Hello World from Spring 4 MVC");
-        return "welcome";
-    }
-
-    @RequestMapping(value = "/helloagain", method = RequestMethod.GET)
-    public String sayHelloAgain(ModelMap model) {
-        model.addAttribute("greeting", "Hello World Again, from Spring 4 MVC");
-        return "welcome";
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(Map<String, Object> model) {
+        return "index";
     }
 
 }
