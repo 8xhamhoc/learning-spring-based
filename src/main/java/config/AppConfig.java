1a0bc33376dac690465c2b54594c7f537ce29d67
8xhamhoc <tomatoquang@gmail.com>
2018-12-04 22:17:41 +0700

    spring configure Environment with annotation

--- src/main/java/config/AppConfig.java
+++ src/main/java/config/AppConfig.java
@@ -1,11 +1,11 @@
 package config;
 
 import org.apache.commons.dbcp.BasicDataSource;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
 
@@ -13,33 +13,22 @@ import javax.sql.DataSource;
 @PropertySource("classpath:application.properties")
 public class AppConfig {
 
-    @Value("${jdbc.driverClassName}")
-    private String driverClassName;
-    @Value("${jdbc.url}")
-    private String jdbcURL;
-    @Value("${jdbc.username}")
-    private String username;
-    @Value("${jdbc.password}")
-    private String password;
+    @Autowired
+    private Environment environment;
 
     @Bean
     public DataSource getDataSource() {
         BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setDriverClassName(driverClassName);
-        basicDataSource.setUrl(jdbcURL);
-        basicDataSource.setUsername(username);
-        basicDataSource.setPassword(password);
+        basicDataSource.setDriverClassName(environment.getProperty("jdbc.driverClassName"));
+        basicDataSource.setUrl(environment.getProperty("jdbc.url"));
+        basicDataSource.setUsername(environment.getProperty("jdbc.username"));
+        basicDataSource.setPassword(environment.getProperty("jdbc.password"));
         return basicDataSource;
     }
 
     @Bean
     public VillageDAO villageDAO() {
         return new VillageDAO(getDataSource());
-    }
-
-    @Bean // Mandatory bean to run @PropertySource("classpath:application.properties")
-    public static PropertySourcesPlaceholderConfigurer placeHolderConfigurer() {
-        return new PropertySourcesPlaceholderConfigurer();
     }
 
 }
