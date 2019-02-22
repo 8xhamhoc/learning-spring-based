e7fc06b6b8dbe5eb9713904356412c20356fe045
8xhamhoc <tomatoquang@gmail.com>
2019-02-22 23:45:53 +0700

    Spring security login form database xml

--- src/main/java/config/SpringSecurityInitializer.java
+++
@@ -1,8 +0,0 @@
-package config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-// Tương đương với load file config trong web.xml
-public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
