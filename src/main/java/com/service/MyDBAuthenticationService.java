c18906eccb9f0ab4591bbcde75ddabd3b6b32948
8xhamhoc <tomatoquang@gmail.com>
2019-03-04 22:58:16 +0700

    Spring security login form database xml - Done

--- src/main/java/com/service/MyDBAuthenticationService.java
+++ src/main/java/com/service/MyDBAuthenticationService.java
@@ -9,12 +9,10 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Service
 public class MyDBAuthenticationService implements UserDetailsService {
 
     @Autowired
